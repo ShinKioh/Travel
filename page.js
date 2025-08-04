37c00fa8309c8727f090685fc5df0431d006d81d
SHIN KI OH <te4328@gmai.com>
2025-08-04 11:18:32 +0900

    update

---
+++ page.js
@@ -0,0 +1,45 @@
+function RivertainHotel() {
+  window.open(
+    'https://www.agoda.com/ko-kr/rivertain-hotel-gyeongju/hotel/gyeongju-si-kr.html?countryId=212&finalPriceView=1&isShowMobileAppPrice=false&cid=1587497&numberOfBedrooms=&familyMode=false&adults=2&children=0&rooms=1&maxRooms=0&checkIn=2025-08-5&isCalendarCallout=false&childAges=&numberOfGuest=0&missingChildAges=false&travellerType=1&showReviewSubmissionEntry=false&currencyCode=KRW&isFreeOccSearch=false&tag=hid9454376%2Cpide449b66e-de46-4a23-81ed-84a2db32f62e&los=1&searchrequestid=3ad1f832-34e9-4db8-b565-42422ce0743d&ds=aAWn44Q3p%2BedvMS8'
+  );
+}
+
+function GGHotel() {
+  window.open(
+    'https://www.agoda.com/ko-kr/gyeongju-tourist-hotel-gg/hotel/gyeongju-si-kr.html?countryId=212&finalPriceView=1&isShowMobileAppPrice=false&cid=1587497&numberOfBedrooms=&familyMode=false&adults=2&children=0&rooms=1&maxRooms=0&checkIn=2025-08-5&isCalendarCallout=false&childAges=&numberOfGuest=0&missingChildAges=false&travellerType=1&showReviewSubmissionEntry=false&currencyCode=KRW&isFreeOccSearch=false&tag=hid529490%2Cpidcdc7e872-5fd6-4c2a-903a-10050ff06fbc&los=1&searchrequestid=1c39320c-bbd1-47ae-88b4-2dd4ca3d8f5e&ds=aiPURZZgHLhbhUwe'
+  );
+}
+
+function DenbastaHeritageHotel() {
+  window.open(
+    'https://www.agoda.com/ko-kr/h34835682/partnerhotel/gyeongju-si-kr.html?cid=1891504&currency=KRW&checkin=2025-08-05&checkout=2025-08-06&numberofchildren=0&mcid=332&masterroomid=903571726&mpt=U2RCRmxFeU1pTlhYcGl4K1NNejMvcXhuVmtlb0hidGkrVGdkcjRaUG5DWTR2MnhSc3dOZ09mRT0&totalprice=123551&totaltax=11600&totalfee=9843&mcur=KRW&hid=34835682&adults=2&rooms=1&los=1&pslc=1&ds=A%2BSVy2hYj1t9hodv'
+  );
+}
+
+function SillaBoutiqueHotel() {
+  window.open(
+    'https://www.agoda.com/ko-kr/silla-boutique-hotel-premium/partnerhotel/gyeongju-si-kr.html?cid=1891504&currency=KRW&checkin=2025-08-05&checkout=2025-08-06&numberofchildren=0&mcid=332&masterroomid=14227044&mpt=RG5vdnJQV3BSZlpwUGhydmZCaENTVkMxVWwrNnFiRlh0TXkxTFY0UGg1am5oaGV2bzNibVVnPT0&totalprice=122418&totaltax=11129&totalfee=0&mcur=KRW&hid=1156249&adults=2&rooms=1&los=1&pslc=1&ds=TdPnwKR2bgHCiTfA'
+  );
+}
+
+function SugarHotel() {
+  window.open(
+    'https://www.agoda.com/ko-kr/sugar-hotel/partnerhotel/gyeongju-si-kr.html?cid=1891504&currency=KRW&checkin=2025-08-05&checkout=2025-08-06&numberofchildren=0&mcid=332&masterroomid=3271702&mpt=dzYzOWJrTUxEaTF6WDZ1OHN1QWlEOWdwV05CVXB0Q1JzM09PcytZclNoOWcwZ0F6T0ExVDlRPT0&totalprice=71444&totaltax=6495&totalfee=0&mcur=KRW&hid=407956&adults=2&rooms=1&los=1&pslc=1&ds=fTJtfEFBfyMgvm2o'
+  );
+}
+
+function PovertaStay() {
+  window.open(
+    'https://www.agoda.com/ko-kr/poverta-stay/partnerhotel/gyeongju-si-kr.html?cid=1891504&currency=KRW&checkin=2025-08-05&checkout=2025-08-06&numberofchildren=0&mcid=332&masterroomid=804786261&mpt=VEdrZmFXSStlazU0by9zS0U3QSt5bzg2Z3R5cWc1SHVaMUFSR0pUenVMaFFCOE9RaVhzaFNiZz0&totalprice=122728&totaltax=10767&totalfee=10533&mcur=KRW&hid=51901111&adults=2&rooms=1&los=1&pslc=1&ds=FvTWXh6BkE%2B%2Boj83'
+  );
+}
+
+function EleganceHotel() {
+  window.open(
+    'https://www.agoda.com/ko-kr/gyeongju-elegance-hotel/partnerhotel/gyeongju-si-kr.html?cid=1891504&currency=KRW&checkin=2025-08-05&checkout=2025-08-06&numberofchildren=0&mcid=332&masterroomid=1011891420&mpt=eWx6QWtwTzJVWG1uY1JrSWg5Y3NTd1NFVnJYMU9seGVtZTQ1T0hUWHRMVDhPeXJaSUQzWnFjMD0&totalprice=98038&totaltax=8570&totalfee=8445&mcur=KRW&hid=61728527&adults=2&rooms=1&los=1&pslc=1&ds=bnHXopytyGogRIvB'
+  );
+}
+
+function test() {
+  window.open('');
+}
---
+++ toggle.js
@@ -0,0 +1,7 @@
+function defaultToggle() {
+  if (document.getElementById('defaultToggle').style.display == 'block') {
+    document.getElementById('defaultToggle').style.display = 'none';
+  } else {
+    document.getElementById('defaultToggle').style.display = 'block';
+  }
+}
